d83884c9fa3b85fa443cc2f00a9503b57013f563
Dave Kessler <Dave.Kessler@sas.com>
2023-04-25 11:38:01 -0400

    Completed module 2

--- ssg/site.py
+++ ssg/site.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -15,3 +16,19 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+                # 'return x if a else None' is not the right behavior
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        # '!=' failed here
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
